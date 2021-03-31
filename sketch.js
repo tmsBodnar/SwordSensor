015d8f45b75045afc1da09a61ccdae20f62a98ad
Tamas Bodnar <t.bodnar@auersignal.com>
2021-03-31 09:01:46 +0200

    handling disconnect

--- sketch.js
+++ sketch.js
@@ -30,29 +30,35 @@ function gotCharacteristics(error, characteristics) {
   // Add a event handler when the device is disconnected
   myBLE.onDisconnected(onDisconnected)
 }
+
+onDisconnected(){
+  isConnected = false;
+}
  
 //          heading = float(list[0]);
 //          pitch = float(list[1]);
 //          roll = float(list[2]);
 
 function draw() {
-  background(255); // set background to white
-  push();          // begin object to draw
-  // variables for matrix translation:
-  let c1 = cos(radians(roll));
-  let s1 = sin(radians(roll));
-  let c2 = cos(radians(pitch));
-  let s2 = sin(radians(pitch));
-  let c3 = cos(radians(heading));
-  let s3 = sin(radians(heading));
-  applyMatrix(c2 * c3, s1 * s3 + c1 * c3 * s2,
-     c3 * s1 * s2 - c1 * s3, 0, -s2, c1 * c2,
-     c2 * s1, 0, c2 * s3, c1 * s2 * s3 - c3 * s1,
-     c1 * c3 + s1 * s2 * s3, 0, 0, 0, 0, 1);
+  if (isConnected) {
+    background(255); // set background to white
+    push();          // begin object to draw
+    // variables for matrix translation:
+    let c1 = cos(radians(roll));
+    let s1 = sin(radians(roll));
+    let c2 = cos(radians(pitch));
+    let s2 = sin(radians(pitch));
+    let c3 = cos(radians(heading));
+    let s3 = sin(radians(heading));
+    applyMatrix(c2 * c3, s1 * s3 + c1 * c3 * s2,
+      c3 * s1 * s2 - c1 * s3, 0, -s2, c1 * c2,
+      c2 * s1, 0, c2 * s3, c1 * s2 * s3 - c3 * s1,
+      c1 * c3 + s1 * s2 * s3, 0, 0, 0, 0, 1);
 
-  // draw arduino board:
-  drawArduino();
-  pop();           // end of object
+    // draw arduino board:
+    drawArduino();
+    pop();           // end of object
+  }
 }
 // draws the Arduino Nano:
 function drawArduino() {
