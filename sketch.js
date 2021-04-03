e754499bc1cff65aa90662c17df193d046ea0767
tms <tms.bodnar@gmail.com>
2021-04-03 08:42:57 +0200

    getInt8 function

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function gotCharacteristics(error, characteristics) {
 
 function gotValue(value){
   var str 	= "";
-  console.log("gotValue: ", value.getInt8(0))
+  console.log("gotValue: ", typeOf(value));
 	//	concat and split string for roll, pitch, yaw (e.g. "-0.58,2.20,328.76")
 		 for(var i=0; i<byteLength; i++){
        console.log('loop',buffer[i]);
